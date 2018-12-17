f379dcc31620b7e225aee13117867655be378232
goolord <zacharyachurchill@gmail.com>
2018-12-17 17:07:17 -0500

    hardcode options, remove toml (unecessary for small script

--- Main.hs
+++ Main.hs
@@ -1,84 +1,54 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
 module Main where
 
 import Network.Wreq
-import Text.Toml
 import Control.Lens
 import Data.Text (Text)
-import Data.Either
-import Data.HashMap.Lazy
-import Text.Megaparsec.Error (errorBundlePretty)
-import Data.Hashable
 import Data.Aeson
 import Data.Aeson.Lens
 import System.Environment
-import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 -- import qualified Data.Aeson as AE
 
 main :: IO ()
 main = do 
-  [path] <- getArgs
-  txt <- T.readFile path
-  let config :: Table
-      config = case parseTomlDoc "" txt of
-        Left err -> error (errorBundlePretty err)
-        Right table -> table
+  [api, lat, long] <- getArgs
   let opts = defaults & param "units" .~ ["auto"]
   r <- getWith opts
         $ "https://api.darksky.net/forecast/" 
-        <> T.unpack (vString (wtr config "api")) <> "/" 
-        <> show (vDouble (wtr config "latitude")) <> "," 
-        <> show (vDouble (wtr config "longitude"))
+        <> api <> "/" 
+        <> lat <> "," 
+        <> long
   let temp :: Double
       (Just (Success temp)) = fromJSON <$> r ^? responseBody . key "currently" . key "temperature"
   let icon' :: Text
       (Just (Success icon')) = fromJSON <$> r ^? responseBody . key "currently" . key "icon"
   let icon :: Text
-      icon = iconEnc (vBool (opt config "nerdfont")) icon'
-  T.putStr $ icon <> " " <> tempEnc (vBool (opt config "round")) temp <> "°"
+      icon = iconEnc icon'
+  T.putStr $ icon <> " " <> tempEnc temp <> "°"
   pure ()
 
-tempEnc :: Bool -> Double -> Text
-tempEnc True  temp = tshow $ (round temp :: Int)
-tempEnc False temp = tshow temp
+tempEnc :: Double -> Text
+tempEnc temp = tshow $ (round temp :: Int)
+{-# INLINE tempEnc #-}
 
-iconEnc :: Bool -> Text -> Text
-iconEnc True "clear-day"           = "\61829" --  
-iconEnc True "clear-night"         = "\61830" --  
-iconEnc True "rain"                = "\61673" --  
-iconEnc True "snow"                = "\64151" -- 流
-iconEnc True "sleet"               = "\58285" --  
-iconEnc True "wind"                = "\61912" --  
-iconEnc True "fog"                 = "\64144" -- 敖 
-iconEnc True "cloudy"              = "\58130" --  
-iconEnc True "partly-cloudy-day"   = "\64148" -- 杖 
-iconEnc True "partly-cloudy-night" = "\58233" -- 
-iconEnc _ _  = ""
-
-wtr :: Table -> Text -> Node
-wtr hm b = table ! b
-  where (VTable table) = hm ! "weather"
-
-opt :: Table -> Text -> Node
-opt hm b = table ! b
-  where (VTable table) = hm ! "options"
-
-vString :: Node -> Text
-vString (VString s) = s
-vString x = error $ "expecting string, got " <> show x
-
-vDouble :: Node -> Double
-vDouble (VFloat d) = d
-vDouble x = error $ "expecting double, got " <> show x
-
-vBool :: Node -> Bool
-vBool (VBoolean b) = b
-vBool x = error $ "expecting bool, got " <> show x
+iconEnc :: Text -> Text
+iconEnc "clear-day"           = "\61829" --  
+iconEnc "clear-night"         = "\61830" --  
+iconEnc "rain"                = "\61673" --  
+iconEnc "snow"                = "\64151" -- 流
+iconEnc "sleet"               = "\58285" --  
+iconEnc "wind"                = "\61912" --  
+iconEnc "fog"                 = "\64144" -- 敖 
+iconEnc "cloudy"              = "\58130" --  
+iconEnc "partly-cloudy-day"   = "\64148" -- 杖 
+iconEnc "partly-cloudy-night" = "\58233" -- 
+iconEnc _ = ""
+{-# INLINE iconEnc #-}
 
 tshow :: Show a => a -> Text
 tshow = T.pack . show
+{-# INLINE tshow #-}
